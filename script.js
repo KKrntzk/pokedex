dc9644c5f18f130e35e809e964e98d47a269b931
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-27 16:39:49 +0100

    capatalizing the first letter of each name

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ function renderEachCard() {
   for (let pokeIndex = 0; pokeIndex < pokemon1.length; pokeIndex++) {
     pokedexTargetRef.innerHTML += getCardTemplate(pokeIndex);
     document.getElementById(`pokeName(${pokeIndex})`).innerHTML =
-      pokemon1[pokeIndex].name;
+      pokemon1[pokeIndex].name.charAt(0).toUpperCase() +
+      pokemon1[pokeIndex].name.substr(1);
     document.getElementById(`pokeId(${pokeIndex})`).innerHTML =
       pokemon1[pokeIndex].index;
     renderEachCardType(pokeIndex);
@@ -56,3 +57,24 @@ function getTypeTemplate(type) {
         <p> ${type.name}</p>
     `;
 }
+
+const colours = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
