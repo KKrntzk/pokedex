1b67bcee184e179c16aa7fc8bafe2f3c3cf13277
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-28 10:14:46 +0100

    type specific color added to cards

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-let pokemon1 = [];
+let pokemonAll = [];
 
 async function fetchPokeData() {
-  for (let i = 0; i < 107; i++) {
+  for (let i = 0; i < 40; i++) {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${i + 387}`
     );
     const eachPokeData = await response.json();
 
-    pokemon1.push({
-      name: eachPokeData.name,
-      index: eachPokeData.id,
-      type: eachPokeData.types,
-      sprite: eachPokeData.sprites.front_default,
-    });
+    pushPokemonIntoArray(eachPokeData);
     renderEachCard();
   }
+}
+
+function pushPokemonIntoArray(eachPokeData) {
+  pokemonAll.push({
+    name: eachPokeData.name,
+    index: eachPokeData.id,
+    type: eachPokeData.types,
+    sprite: eachPokeData.sprites.front_default,
+  });
 }
 
 function renderEachCard() {
   const pokedexTargetRef = document.getElementById("pokemonCardsTarget");
   pokedexTargetRef.innerHTML = "";
-  for (let pokeIndex = 0; pokeIndex < pokemon1.length; pokeIndex++) {
+  for (let pokeIndex = 0; pokeIndex < pokemonAll.length; pokeIndex++) {
     pokedexTargetRef.innerHTML += getCardTemplate(pokeIndex);
     document.getElementById(`pokeName(${pokeIndex})`).innerHTML =
-      pokemon1[pokeIndex].name.charAt(0).toUpperCase() +
-      pokemon1[pokeIndex].name.substr(1);
+      pokemonAll[pokeIndex].name.charAt(0).toUpperCase() +
+      pokemonAll[pokeIndex].name.substr(1);
     document.getElementById(`pokeId(${pokeIndex})`).innerHTML =
-      pokemon1[pokeIndex].index;
+      pokemonAll[pokeIndex].index;
     renderEachCardType(pokeIndex);
   }
 }
 
 function getCardTemplate(pokeIndex) {
   return /*html*/ `
-    <div class="single-cards">
+    <div id="singleCard(${pokeIndex})" class="single-cards">
     <img class="poke-sprite" id="pokeSprite(${pokeIndex})" src=""></img>
     <p  id="pokeName(${pokeIndex})"></p>
     <p id="pokeId(${pokeIndex})"></p>
@@ -44,12 +48,20 @@ function getCardTemplate(pokeIndex) {
 
 function renderEachCardType(pokeIndex) {
   let typeTemplate = document.getElementById(`pokeType(${pokeIndex})`);
-  for (let j = 0; j < pokemon1[pokeIndex].type.length; j++) {
-    typeTemplate.innerHTML += getTypeTemplate(pokemon1[pokeIndex].type[j].type);
+  for (let j = 0; j < pokemonAll[pokeIndex].type.length; j++) {
+    typeTemplate.innerHTML += getTypeTemplate(
+      pokemonAll[pokeIndex].type[j].type
+    );
+    document
+      .getElementById(`singleCard(${pokeIndex})`)
+      .classList.add(`type-${pokemonAll[pokeIndex].type[j].type.name}`);
+
+    if (pokemonAll.type) {
+    }
   }
   document
     .getElementById(`pokeSprite(${pokeIndex})`)
-    .setAttribute("src", pokemon1[pokeIndex].sprite);
+    .setAttribute("src", pokemonAll[pokeIndex].sprite);
 }
 
 function getTypeTemplate(type) {
