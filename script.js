12aebba72a48e00ddce87d78a351f56d03345997
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-28 09:50:35 +0100

    background css classes added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let pokemon1 = [];
 
 async function fetchPokeData() {
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 107; i++) {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${i + 387}`
     );
@@ -57,24 +57,3 @@ function getTypeTemplate(type) {
         <p> ${type.name}</p>
     `;
 }
-
-const colours = {
-  normal: "#A8A77A",
-  fire: "#EE8130",
-  water: "#6390F0",
-  electric: "#F7D02C",
-  grass: "#7AC74C",
-  ice: "#96D9D6",
-  fighting: "#C22E28",
-  poison: "#A33EA1",
-  ground: "#E2BF65",
-  flying: "#A98FF3",
-  psychic: "#F95587",
-  bug: "#A6B91A",
-  rock: "#B6A136",
-  ghost: "#735797",
-  dragon: "#6F35FC",
-  dark: "#705746",
-  steel: "#B7B7CE",
-  fairy: "#D685AD",
-};
